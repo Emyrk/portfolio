ba3a83ade36c8f47610a1eae14b08fa8afbd158d
emyrk <stevenmasley@gmail.com>
2018-12-23 03:10:44 -0500

    Added modal popping

--- _javascript/index.js
+++ _javascript/index.js
@@ -9,3 +9,24 @@ $(".tag").on('click', function (event) {
     $(".tile.is-child:has(.tag." + id + ")").css("opacity", 1)
     $(".tag_X").show()
 });
+
+$(".tile.is-child").on('click', function(e) {
+    if ($(e.target).hasClass("tag")) {
+        return
+    }
+
+    if ($(e.target).closest(".modal").length > 0) {
+        return
+    }
+
+    m = $(this).find(".modal")
+    m.addClass("is-active")
+})
+
+$(".modal-background").on('click', function(e){
+    $(this).parent().removeClass("is-active")
+})
+
+$(".modal-delete").on('click', function(e){
+    $(this).closest(".modal").removeClass("is-active")
+})
